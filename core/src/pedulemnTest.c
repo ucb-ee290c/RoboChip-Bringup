8aea45a886f0134215afddc4e0d1c5f446737775
Marco-Xuanlin-Chen <chenxuanlin2001@berkeley.edu>
2023-12-04 19:47:44 -0800

    setup pedulum demo

---
+++ core/src/pedulemnTest.c
@@ -0,0 +1,14 @@
+#include "rocc.h"
+#include "bitcasts.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int pedulemn_test() {
+    //position, velocity, angle, angular velocity
+    float vec[4] = [0.5, 0, 0, 0]
+    float 
+}
